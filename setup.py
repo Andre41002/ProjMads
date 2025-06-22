eca7a8c0f38c139de5d3616323e5c473f2b1c5a2
Andre41002 <153658739+Andre41002@users.noreply.github.com>
2025-06-22 18:08:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gestao-alunos',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=['tabulate'],
     author='Grupo 2 MADS 2ano',
