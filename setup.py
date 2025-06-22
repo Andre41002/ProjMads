48175be9081914becfdef008e820ee1bd05f5c43
Andre41002 <a041002@ipmaia.pt>
2025-06-22 18:24:17 +0100

    Update setup.py
    a

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
+    requires=["setuptools","wheel"],
+    build_backend = "setuptools.build_meta",
     name='gestao-alunos',
     version='0.1.1',
     packages=find_packages(),
@@ -10,6 +12,8 @@ setup(
     description='Sistema de gestão de alunos, docentes, turmas e disciplinas.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/seuusuario/gestao-alunos',    
+    url='https://github.com/a041002/gestao-alunos',    
     python_requires='>=3.6',
+    
+
 )
